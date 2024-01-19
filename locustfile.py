6d2defdeafdb237e858b16020edeb95d8c03f5d2
Nitin Prakash <nitin110010@gmail.com>
2024-01-19 10:51:33 -0500

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -1,11 +1,62 @@
-from locust import HttpUser, task, between
+import random
+import time
 
-class QuickstartUser(HttpUser):
-    wait_time = between(0, 1)
+from locust import HttpUser, between, task
 
-    def on_start(self):
-        self.client.headers['User-Agent'] = "Mozilla/5.0"
+
+class ChatUser(HttpUser):
+    wait_time = between(0, 0)
 
     @task
-    def index_page(self):
-        self.client.get("/")
+    def ask_question(self):
+        self.client.get("/", stream=True)
+        #time.sleep(5)
+        self.client.post(
+            "/conversation",
+            json={
+                "messages": [
+                    {
+                        "content": random.choice(
+                            [
+                                "What is business analysis?",
+                                "What is mind mapping?",
+                                "What are some techniques published in the BABOK guide?",
+                            ]
+                        ),
+                        "role": "user",
+                    },
+                ],
+                "context": {
+                    "overrides": {
+                        "retrieval_mode": "hybrid",
+                        "semantic_ranker": True,
+                        "semantic_captions": False,
+                        "top": 3,
+                        "suggest_followup_questions": False,
+                    },
+                },
+            },
+        )
+#        time.sleep(5)
+        self.client.post(
+            "/conversation",
+            json={
+                "messages": [
+                    {"content": "What is business analysis?", "role": "user"},
+                    {
+                        "content": "Business analysis is the practice of enabling change in an enterprise by defining needs and recommending solutions that deliver value to stakeholders. It involves understanding the current state of the organization, identifying areas for improvement, and proposing changes to achieve desired outcomes . Business analysts are responsible for discovering, synthesizing, and analyzing information from various sources within an enterprise, including tools, processes, documentation, and stakeholders. They play a crucial role in aligning the designed and delivered solutions with the needs of stakeholders. The activities performed by business analysts include understanding enterprise problems and goals, analyzing needs and solutions, devising strategies, driving change, and facilitating stakeholder collaboration. Business analysis can be applied in various domains and industries, and it is not limited to a specific job title or organizational role.",
+                        "role": "assistant",
+                    },
+                    {"content": "How can business analysis be applied to an organizational digital transformation?", "role": "user"},
+                ],
+                "context": {
+                    "overrides": {
+                        "retrieval_mode": "hybrid",
+                        "semantic_ranker": True,
+                        "semantic_captions": False,
+                        "top": 3,
+                        "suggest_followup_questions": False,
+                    },
+                },
+            },
+        )
